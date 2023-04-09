e3f8da8a99a9cb099d13f6dd5d235ff12cb96296
Antonina Shchetinkina <a.shetinkina@id-mt.ru>
2023-04-09 13:30:51 +0400

    Добавлена миграция

---
+++ backend/db/migrations/20221118084654_create_sentence_types.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class CreateSentenceTypes extends AbstractMigration
+{
+
+    public function change()
+    {
+        $types = $this->table('sentence_types', ['id' => false, 'primary_key' => 'type_id']);
+        $types->addColumn('type_id', 'integer', ['limit' => 11, 'null' => false, 'identity' => true, 'comment' => 'id типа'])
+            ->addColumn('title', 'string', ['limit' => 255, 'null' => false, 'comment' => 'Название типа'])
+            ->create();
+
+        $types->insert([
+            'title' => 'Вопросы',
+        ])->insert([
+            'title' => 'Повествовательные',
+        ])->insert([
+            'title' => 'Вопросы с сложным подлежащим',
+        ])->insert([
+            'title' => 'Повествовательные с сложным подлежащим',
+        ])->save();
+    }
+}
